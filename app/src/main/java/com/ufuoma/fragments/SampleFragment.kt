a4d0dd79a345ce97243cf497af7319d7377939c4
ufuoma86 <ufuomaavwata@gmail.com>
2021-07-14 17:53:36 +0100

    Initial commit

---
+++ app/src/main/java/com/ufuoma/fragments/SampleFragment.kt
@@ -0,0 +1,12 @@
+package com.ufuoma.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SampleFragment:  Fragment(R.layout.sample_fragment) {
+
+
+}
